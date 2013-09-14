fab88cc1cbe2f6438b50ed54343181eaa3403406
Tilak <tilak@robotilak.com>
2013-09-15 03:28:14 +0530

    RFID is serial Read
    The RFID was connected to ATMEGA 2560 and the TAG names were read using
    this code .

---
+++ RFID_READER_WORKING.ino
@@ -0,0 +1,26 @@
+/* RFID Tag reader cc robotilak.com  
+The module use is a EM18 reader module 
+EM 18 RS232 pin to TX of ATMega 2560.
+GND common 
+
+*/
+
+
+char val = 0; // variable to store the ave  h from the serial port
+
+
+void setup() 
+  {
+    Serial.begin(9600); // connect to the serial port
+  }
+
+
+void loop () 
+{
+    // read the serial port
+    if(Serial.available() > 0) 
+    {
+      val = Serial.read();
+      Serial.write(val);
+    }
+}
